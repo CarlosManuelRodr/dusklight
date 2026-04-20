e5c7dfdedd8945f31fae87882d4d52f47884d6c4
madeline <qwertytrogi@gmail.com>
2026-04-20 08:04:05 -0700

    maybe fix apple ci idk this shit is giga fucked

--- cmake/fix_capstone_policy.cmake
+++ cmake/fix_capstone_policy.cmake
@@ -1,7 +1,13 @@
-# Removes cmake_policy(SET CMP0048 OLD) from capstone 4.0.2 CMakeLists.txt
+# Patches capstone's CMakeLists.txt for compatibility with CMake >= 4.0:
+#   - Removes cmake_policy(SET CMP0048 OLD) (rejected by CMake >= 3.27)
+#   - Bumps cmake_minimum_required to 3.5 (CMake >= 4.0 dropped < 3.5 support)
 file(READ "${DIR}/CMakeLists.txt" _content)
 string(REPLACE
     "cmake_policy(SET CMP0048 OLD)"
     "# cmake_policy(SET CMP0048 OLD)"
+    _content "${_content}")
+string(REGEX REPLACE
+    "cmake_minimum_required\\(VERSION [0-9]+\\.[0-9]+(\\.[0-9]+)?\\)"
+    "cmake_minimum_required(VERSION 3.5)"
     _content "${_content}")
 file(WRITE "${DIR}/CMakeLists.txt" "${_content}")
